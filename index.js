9515147dd1cfd9d72ac622fa4f97080b471a78bc
nbansal0609 <114054159+nbansal0609@users.noreply.github.com>
2023-11-12 15:18:23 -0500

    new changes

--- index.js
+++ index.js
@@ -7,19 +7,25 @@ const io = new Server(server);
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
- 
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-      io.emit('chat message', msg);
-    });
+
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 
-  
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
